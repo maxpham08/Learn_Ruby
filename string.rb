a9d8f4dbb0769f8cee23b81f8ab2610bc84f877d
Hien Pham <vhpham80@gmail.com>
2021-07-10 18:46:05 +0700

    string manipulation examples

--- string.rb
+++ string.rb
@@ -11,3 +11,15 @@ p first_name + " " + last_name
 # string interpolation
 
 p "My first name is #{first_name} and my last name is #{last_name}"
+
+# methods, how to find them
+p first_name.class
+p 10.class
+
+full_name = first_name + " " + last_name
+p full_name.length
+
+sentence = "Welcome to the jungle"
+
+p sentence.sub("the jungle", "utopia")
+p sentence
