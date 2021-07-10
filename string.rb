85929a13070334523a9b2923468050717e6a3f1d
Hien Pham <vhpham80@gmail.com>
2021-07-10 18:27:28 +0700

    string concatenation and interpolation example

---
+++ string.rb
@@ -0,0 +1,13 @@
+# string concatenation
+first_name = "Max"
+last_name = "Pham"
+
+sentence = "My name is Max"
+
+p sentence
+
+p first_name + " " + last_name
+
+# string interpolation
+
+p "My first name is #{first_name} and my last name is #{last_name}"
