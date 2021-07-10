ed45c86cb94724ba480e48e0d07b55421897a4d9
Hien Pham <vhpham80@gmail.com>
2021-07-10 19:02:47 +0700

    number manipulation examples

---
+++ numbers.rb
@@ -0,0 +1,16 @@
+p 1 + 2
+
+p 10 / 4.to_f
+
+p "5".to_i + "11".to_f
+
+print "Simple calculator\n"
+20.times { print "-"}
+
+p "Enter your first number"
+number1 = gets.chomp
+
+p "Enter your second number"
+number2 = gets.chomp
+
+p number1.to_i + number2.to_i
