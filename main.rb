5619004802cdd0cd9658fdd340f7616909d90dcb
Hien Pham <vhpham80@gmail.com>
2021-07-10 18:20:32 +0700

    test ruby method

--- main.rb
+++ main.rb
@@ -1,5 +1,23 @@
+#
 address = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 p address
 
 new_address = address.reverse
 p new_address
+
+print address
+
+print "\nHello"
+
+def say_hello
+    print "\ntest method"
+end    
+
+say_hello
+
+def say_hello(thing_to_say)
+    print "\nHello "
+    print thing_to_say
+end
+
+say_hello("hien")
