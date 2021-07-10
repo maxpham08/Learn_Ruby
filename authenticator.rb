2c8acbc3e8666944831d12dc4838cec7f99286a3
Hien Pham <vhpham80@gmail.com>
2021-07-10 22:28:30 +0700

    support looping

--- authenticator.rb
+++ authenticator.rb
@@ -9,24 +9,30 @@ users = [
 p "Welcome to the authenticator"
 30.times { print "-"}
 
-p ""
-p "This program will take input from the user and compare password"
-print "Username: "
-user_name = gets.chomp
+while true
+    p " "
+    p "This program will take input from the user and compare password"
+    print "Username: "
+    user_name = gets.chomp
 
-print "Password: "
-password = gets.chomp
+    print "Password: "
+    password = gets.chomp
 
-p "Your username is #{user_name} and password is #{password}"
+    p "Your username is #{user_name} and password is #{password}"
 
-user = users.select { | user | user[:username] == user_name}.first
+    user = users.select { | user | user[:username] == user_name}.first
 
-if user == nil
-    p "there is no user"
-else
-    if user[:password] == password
-        p "Great, your password is correct!!!"
+    if user == nil
+        p "there is no user"
     else
-        p "Sorry, please try a gain"
-    end
-end    
+        if user[:password] == password
+            p "Great, your password is correct!!!"
+        else
+            p "Sorry, please try a gain"
+        end
+    end    
+
+    p "Press q to quit or anykey to continue"
+    input = gets.chomp.downcase
+    break if input == "q"
+end
